d83c3d964c5510792a945d6879b297e89797ea1d
Brian <scalab13@students.rowan.edu>
2023-02-15 15:10:04 -0500

    Changes

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -1,8 +1,8 @@
 /*
  * Part2.c
  *
- *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *  Created on: Feb 15, 2023
+ *      Author: Brian Scala
  *
  *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
  */
@@ -29,15 +29,25 @@ void main(){
 
 
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
+    // Configure RED LED on P1.0 as Output
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
 
-    // @TODO Initialize Button 2.3
-
+    // Configure Button on P2.3 as input with pullup resistor
+    P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+    P2REN |= BIT3;                          // P2.3 pull-up register enable
+    P2IES |= BIT3;                         // P2.3 Low --> High edge
+    P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
 
+
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
+    // Timer1_B3 setup
+       TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+       TB1CCR0 = 5000;
+       TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
 
 }
 
@@ -51,6 +61,7 @@ void timerInit(){
 __interrupt void Port_2(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+    // Timer B1 interrupt service routine
 
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
 
