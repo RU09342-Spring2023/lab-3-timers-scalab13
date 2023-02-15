ab82f3bc180901e1b503ce5bf07012ef543a698d
Brian <scalab13@students.rowan.edu>
2023-02-15 14:16:52 -0500

    Part 1

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -1,8 +1,8 @@
 /*
  * Part1.c
  *
- *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *  Created on: Feb 15, 2023
+ *      Author: Brian Scala
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
@@ -66,7 +66,7 @@ void gpioInit(){
       // Configure Button on P2.3 as input with pullup resistor
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
       P2REN |= BIT3;                          // P2.3 pull-up register enable
-      P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+      P2IES |= BIT3;                         // P2.3 Low --> High edge
       P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
@@ -78,18 +78,8 @@ void gpioInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
-
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
-    {
-        LED_Color = 0;
-        // @TODO Add code to change which edge the interrupt should be looking for next
-    }
-
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
-    {
-        LED_Color = 1;
-        // @TODO Add code to change which edge the interrupt should be looking for next
-    }
+    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
+    LED_Color ^= BIT0;                  // Toggle Color
+    P2IES = ~P2IES;                         // Switch between checking falling and rising edge
 }
 
